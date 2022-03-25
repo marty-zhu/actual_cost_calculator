d5eb1e9c0a65f837854da2e6735f79ff17790948
marty-zhu <marty.zhu@gmail.com>
2022-03-24 20:59:26 -0700

    initial commit with existing files

---
+++ actualcost.js
@@ -0,0 +1,29 @@
+const actualCost = document.querySelector('span#actual-cost');
+
+function getValues() {
+    let inputs = document.getElementsByClassName('text-input-field')
+    globalThis.values = {};
+    for (let input of inputs) {
+        values[input.id] = parseFloat(input.value);
+    }
+    values['time'] = values['time'] / 60;
+    let tripType = document.querySelector('input[type="radio"]:checked').value;
+    if (tripType == 1) {
+        values['distance'] *= 2;
+        values['time'] *= 2;
+    }
+    console.log(values);
+}
+
+function convertToCostPerMile() {
+    return costPerMile = (1 / values['fueleconomy']) * values['gasprice'];
+}
+
+function calActualCost() {
+    getValues();
+    let costPerMile = convertToCostPerMile();
+    let tripTotalCost = costPerMile * values['distance'] + values['other'];
+    let myOppotunityCost = values['myhourlyrate'] * values['time'];
+    let finalCost = tripTotalCost + myOppotunityCost - values['offsets'] + values['itemcost'];
+    actualCost.innerText = finalCost.toFixed(2);
+}
